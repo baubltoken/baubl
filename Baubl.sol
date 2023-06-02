a69a798d2137e26ab10d9bd8700a96ff5cbc04a7
BaublToken <134706347+baubltoken@users.noreply.github.com>
2023-06-02 13:39:50 +0200

    Update Baubl.sol

--- Baubl.sol
+++ Baubl.sol
@@ -8,9 +8,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /*
    Baubl project
 
-   Web: https://sites.google.com/view/baubltoken
+   Web: https://www.baubl.io
    Contract: https://etherscan.io/address/0xcbd514651067342047cd21b4e4dd07cd1152d3fb
-   Twitter: @BaublToken
+   Twitter: @BaublToken (https://twitter.com/baubletoken)
 
 */
 contract Baubl is ERC20, ERC20Burnable, Ownable {
